cbe92b13dd98c31f8f1582be1afd87e9eb29a1a5
Keren <pkvega@uninorte.edu.co>
2026-09-13 16:52:14 -0500

    busqueda y eliminacion

---
+++ Assets/Scripts/ArbolABB.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+
+public class ArbolABB<T> where T : IComparable<T>
+{
+    public Nodo<T> Raiz { get; private set; }
+
+    public void Insertar(T valor)
+    {
+        Raiz = InsertarRec(Raiz, valor);
+    }
+
+    private Nodo<T> InsertarRec(Nodo<T> nodo, T valor)
+    {
+        if (nodo == null)
+            return new Nodo<T>(valor);
+
+        int comparacion = valor.CompareTo(nodo.Valor);
+        if (comparacion < 0)
+            nodo.Izquierdo = InsertarRec(nodo.Izquierdo, valor);
+        else if (comparacion > 0)
+            nodo.Derecho = InsertarRec(nodo.Derecho, valor);
+
+        return nodo;
+    }
+        public bool Buscar(T valor)
+    {
+        return BuscarRec(Raiz, valor);
+    }
+
+    private bool BuscarRec(NodoABB<T> nodo, T valor)
+    {
+        if (nodo == null) return false;
+
+        int comparacion = valor.CompareTo(nodo.Valor);
+        if (comparacion == 0) return true;
+           if(comparacion < 0)
+                return BuscarRec(nodo.Izquierdo, valor);
+            else
+                return BuscarRec(nodo.Derecho, valor);
+        public void Eliminar(T valor)
+    {
+        Raiz = EliminarRec(Raiz, valor);
+    }
+
+    private NodoABB<T> EliminarRec(NodoABB<T> nodo, T valor)
+    {
+        if (nodo == null) return null;
+
+        int comparacion = valor.CompareTo(nodo.Valor);
+        if (comparacion < 0)
+            nodo.Izquierdo = EliminarRec(nodo.Izquierdo, valor);
+        else if (comparacion > 0)
+            nodo.Derecho = EliminarRec(nodo.Derecho, valor);
+        else
+        {
+            if (nodo.Izquierdo == null) return nodo.Derecho;
+            if (nodo.Derecho == null) return nodo.Izquierdo;
+
+            NodoABB<T> sucesor = ObtenerMinimo(nodo.Derecho);
+            nodo.Valor = sucesor.Valor;
+            nodo.Derecho = EliminarRec(nodo.Derecho, sucesor.Valor);
+        }
+        return nodo;
+    }
+
+    private NodoABB<T> ObtenerMinimo(NodoABB<T> nodo)
+    {
+        while (nodo.Izquierdo != null)
+            nodo = nodo.Izquierdo;
+        return nodo;
+    }
+}
