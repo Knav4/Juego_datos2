aa68287dcece0aa1f203369301587db7a07e6b27
Keren <pkvega@uninorte.edu.co>
2026-09-13 15:57:57 -0500

    clase Nodo para representar los nodos del arbol

---
+++ Assets/Scripts/Nodo.cs
@@ -0,0 +1,12 @@
+Public class Nodo{
+    public int valor;
+    Public Nodo izquierda;
+    public Nodo derecha;
+
+    public Nodo(int valor){
+        this.valor=valor;
+        izquierda = null;
+        derecha=null;
+    }
+    
+}
